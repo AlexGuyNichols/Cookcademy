d28a4104fa3bfa7041fe843cfd4bb08a2fce9e09
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-08 13:34:38 +0100

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Alex Nichols on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
