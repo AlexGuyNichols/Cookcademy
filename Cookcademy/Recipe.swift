21e3fadf008424dc58f702b568933a7c742d0039
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-09 11:07:29 +0100

    Updated Recipe.swift

--- Cookcademy/Recipe.swift
+++ Cookcademy/Recipe.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Recipe {
+	var name: String
+	var description: String
+	var author: String
+	var category: String
+	var ingredients: [String]
+	var directions: [String]
+}
+
