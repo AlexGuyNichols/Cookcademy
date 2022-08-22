96b2176cf30ba0608be9bd2af9872f2bedf46fa7
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-22 18:32:31 +0100

    Added the ability to add recipes & also made sure empty ones can't be added

--- Cookcademy/Views/ExploreRecipes/RecipesListView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipesListView.swift
@@ -10,19 +10,54 @@ import SwiftUI
 struct RecipesListView: View {
 	@EnvironmentObject private var recipeData: RecipeData
 	let category: MainInformation.Category
+	
+	@State private var isPresenting = false
+	@State private var newRecipe = Recipe()
 
 	private let listBackgroundColor = AppColor.background
 	private let listTextColor = AppColor.foreground
 	
 	var body: some View {
-		List {
-			ForEach(recipes) { recipe in
-				NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+		NavigationView {
+			List {
+				ForEach(recipes) { recipe in
+					NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+				}
+				.listRowBackground(listBackgroundColor)
+				.foregroundColor(listTextColor)
 			}
-			.listRowBackground(listBackgroundColor)
-			.foregroundColor(listTextColor)
+			.navigationTitle(navigationTitle)
+			.toolbar(content: {
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Button(action: {
+						isPresenting = true
+					}, label: {
+						Image(systemName: "plus")
+					})
+				}
+			})
+			.sheet(isPresented: $isPresenting, content: {
+				NavigationView {
+					ModifyRecipeView(recipe: $newRecipe)
+						.toolbar(content: {
+							ToolbarItem(placement: .cancellationAction) {
+								Button("Dismiss") {
+									isPresenting = false
+								}
+							}
+							ToolbarItem(placement: .confirmationAction) {
+								if newRecipe.isValid {
+									Button("Add") {
+										recipeData.add(recipe: newRecipe)
+										isPresenting = false
+									}
+								}
+							}
+						})
+						.navigationTitle("Add a New Recipe")
+				}
+			})
 		}
-		.navigationTitle(navigationTitle)
 	}
 }
 
